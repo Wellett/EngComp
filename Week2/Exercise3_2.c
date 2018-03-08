333460396b4316475075653b72c5cc3ed49ce79a
William <wellett@student.unimelb.edu.au>
2018-03-09 00:26:27 +1100

    Finished week 2 prep

--- Week2/Exercise3_2.c
+++ Week2/Exercise3_2.c
@@ -8,7 +8,13 @@ Exercise 3.2 From Programming, Problem Solving and Abstraction with C
 #include <stdlib.h>
 #include <string.h>
 
+/* FUNCTION PROTOTYPES */
 void exA();
+void exB();
+void exC();
+void exD();
+void exE();
+void exF();
 
 int
 main(int argc, char *argv[]){
@@ -17,7 +23,22 @@ main(int argc, char *argv[]){
     printf("Please select example a, b, c, d, e, or f");
   }
   else if (strcmp(argv[1], "a") == 0){
-      exA();
+    exA();
+  }
+  else if (strcmp(argv[1], "b") == 0){
+    exB();
+  }
+  else if (strcmp(argv[1], "c") == 0){
+    exC();
+  }
+  else if (strcmp(argv[1], "d") == 0){
+    exD();
+  }
+  else if (strcmp(argv[1], "e") == 0){
+    exE();
+  }
+  else if (strcmp(argv[1], "f") == 0){
+    exF();
   }
   else{
     printf("Invalid argument\nProgram terminating");
@@ -56,9 +77,51 @@ void exB(){
 }
 
 void exC(){
+  int days;
   int month = 7;
   if (month == 2) {
     days = 28;
   }
-  
+  else if (month == 4 || 6 ||9||11){
+    days = 30;
+  }
+  else {
+    days = 31;
+  }
+  printf("days=%d,\n", days);
+}
+
+void exD(){
+  int x = 1,  y = 2;
+  if (x > y)
+    printf("x = %d, y = %d\n", x, y);
+    x = x + 1;
+  if (x < y)
+    printf("x = %d, y = %d\n", x, y);
+    y = y + 2;
+    printf("x = %d, y = %d\n", x, y);
+}
+
+void exE(){
+  int x = 1, y = 2;
+  if (x>y); {
+    printf("x = %d, y = %d\n", x, y);
+    x = x + 1;
+  }
+  if (x<y); {
+    printf("x = %d, y = %d\n", x, y);
+    y = y + 2;
+  }
+}
+
+void exF(){
+  int x = 0, y = 1;
+  if (y < x){
+    printf("y is smaller\n");
+  } else if (y = x) {
+    printf("x and y are equal\n");
+  } else {
+    printf("y is greater\n");
+  }
+
 }
